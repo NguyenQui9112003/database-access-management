d430cafa3c37a8fd337829dc2adec9cb122f0f0d
NguyenQui9112003 <147489314+NguyenQui9112003@users.noreply.github.com>
2024-11-28 23:38:06 +0700

    commit project structure

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
+import java.sql.Connection;
+
+import org.example.config.DatabaseConfig;
+import org.example.connection.PostgresConnectionFactory;
+import org.example.service.DatabaseService;
+import org.example.service.DatabaseServiceConcrete;
+import org.example.service.PostgresDatabaseServiceConcrete;
+import org.example.entity.User;
 
 public class Main {
     public static void main(String[] args) {
+        DatabaseConfig dbConfig = new DatabaseConfig("jdbc:postgresql://localhost:5432",
+                "DAM-Framework","postgres","123456");
+        PostgresConnectionFactory pgConnection = new PostgresConnectionFactory();
+        Connection con = pgConnection.createConnection(dbConfig);
 
+        // Tạo factory và service cho PostgreSQL
+        DatabaseServiceConcrete factory = new PostgresDatabaseServiceConcrete();
+        DatabaseService dbService = factory.createDatabaseService(con);
+
+        // Tạo bảng cho entity User
+        dbService.createTable(User.class);
+        dbService.closeConnection(con);
     }
 }
