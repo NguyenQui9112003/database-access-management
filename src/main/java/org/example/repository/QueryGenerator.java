8008e090a44ea66d676843b5812ae216cefdcae6
qthai2210 <122585540+qthai2210@users.noreply.github.com>
2024-12-02 14:29:05 +0700

    Implement  operations for User entity including insert and update

--- src/main/java/org/example/repository/QueryGenerator.java
+++ src/main/java/org/example/repository/QueryGenerator.java
@@ -3,4 +3,8 @@ package org.example.repository;
 public interface QueryGenerator {
     String createTableQuery(Class<?> clazz);
     // crud
+    String insertQuery(Object entity);
+
+    String updateQuery(Object entity);
+    
 }
